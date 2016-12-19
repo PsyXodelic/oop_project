2a083d411a62015ec319cd572c9f45e084c005d5
PsyXodelic <beavkostya@gmail.com>
2016-12-19 22:13:20 +0300

    Update Subject.java

--- src/com/company/Subject.java
+++ src/com/company/Subject.java
@@ -1,8 +1,5 @@
 package com.company;
 
-/**
- * Created by op on 31.05.2016.
- */
 public interface Subject {
     public void registerObserver(Observer o);
     public void removeObserver(Observer o);
